650b046941a72f1589ba607795976772d48836d3
student <student@ASU-1207-21>
2026-09-07 14:59:25 +0300

    сделал структуры трубы и станции

---
+++ dudukinA233/dudukinA233/dudukinA233.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    struct Station
+    {
+        string Name;
+        int count_ALL;
+        int count_work;
+        char class_station; // (a,b,c)
+        bool rabota;
+    };
+    }
+
+    struct Pipe
+    {
+        string KM;
+        float lenght;
+        float d;
+        bool remont;
+    };
+
+
+
+    return 0;
+}
+
