2d02718e7a1ee3f16581a9f9df51e4a2f71aeb87
USER <dudukinartoym2324@gmail.com>
2026-09-20 22:02:36 +0300

    добавил функцию Add_P() (функция добавления трубы)

--- dudukinA233/dudukinA233/dudukinA233.cpp
+++ dudukinA233/dudukinA233/dudukinA233.cpp
@@ -18,4 +18,46 @@ struct Station
     int count_in_work;
     char class_station;             // (a,b,c)
 };
+Pipe Add_P()
+{
+    Pipe p;
+    cout << "1.Kilometer marker" << endl;
+    cin >> p.Name_KM;
+    do {
+        cout << "2.enter length" << endl;
+        cin >> p.length;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(1000, '\n');
+        }
+    } while (cin.fail() || p.length <= 0);
 
+    do {
+        cout << "3. enter diametr pipe" << endl;
+        cin >> p.d;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(1000, '\n');
+        }
+    } while (cin.fail() || p.d <= 0);
+    int repair;
+    do
+    {
+        cout << "4. Status under repair (1 or 0)" << endl;
+        cin >> repair;
+
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+
+    } while (cin.fail() || (repair != 0 && repair != 1));
+
+    p.repair = repair;
+
+
+    return p;
+}
